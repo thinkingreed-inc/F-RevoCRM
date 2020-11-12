2c6db3847f07d0d7d6392b8cb9e4ea0305874fc1
Remicck <remicck@gmail.com>
2020-11-12 15:53:43 +0900

    日報モジュールが登録されていない不具合の修正

--- modules/Migration/schema/730_to_731.php
+++ modules/Migration/schema/730_to_731.php
@@ -20,6 +20,12 @@ if (defined('VTIGER_UPGRADE')) {
 	$db->query("alter table vtiger_faq change question question longtext;");
 	$db->query("alter table vtiger_faq change answer answer longtext;");
 
+	// 日報モジュールがない場合に追加する
+	$result = $db->query("select * from vtiger_tab where name = 'Dailyreports'");
+	$count = $db->num_rows($result);
+	if($count == 0){
+		include_once("setup/scripts/01_Make_Dailyreports.php");
+	}
 	// 日報のコメントの文字数制限を変更
 	$db->query("alter table vtiger_dailyreports change dailyreportscomment dailyreportscomment longtext;");
 }
