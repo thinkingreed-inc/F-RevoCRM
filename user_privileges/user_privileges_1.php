a0cbbd03e236de4f6a672bb45c5ac1d1eaf5b20d
Remicck <remicck@gmail.com>
2020-11-16 16:20:11 +0900

    user_privileges系のファイルを入れてフォルダが生成されるように設定

---
+++ user_privileges/user_privileges_1.php
@@ -0,0 +1,9 @@
+<?php
+
+
+//This is the access privilege file
+$is_admin=true;
+
+$user_info=array('user_name'=>'admin','is_admin'=>'on','user_password'=>'$2y$10$K349YtdYPDEy/ixXMBMBZeFCPgvnOIyo9gABHEUuHn.mvD54UrqMS','confirm_password'=>'$2y$10$m6F0FhXVGkAAuRM4a.GXou1a6XopQFUKGz2j0FtnJ3OFLY8iD1Wmy','first_name'=>'','last_name'=>'システム管理者','roleid'=>'H2','email1'=>'admin@example.com','status'=>'Active','activity_view'=>'This Week','lead_view'=>'Today','hour_format'=>'am/pm','end_hour'=>'23:00','start_hour'=>'08:00','is_owner'=>'','title'=>'','phone_work'=>'','department'=>'','phone_mobile'=>'','reports_to_id'=>'','phone_other'=>'','email2'=>'','phone_fax'=>'','secondaryemail'=>'','phone_home'=>'','date_format'=>'yyyy-mm-dd','signature'=>'','description'=>'','address_street'=>'','address_city'=>'','address_state'=>'','address_postalcode'=>'','address_country'=>'','accesskey'=>'gGD8kzRmzeetyjgL','time_zone'=>'Asia/Tokyo','currency_id'=>'1','currency_grouping_pattern'=>'123,456,789','currency_decimal_separator'=>'.','currency_grouping_separator'=>',','currency_symbol_placement'=>'$1.0','imagename'=>'','internal_mailer'=>'1','theme'=>'alphagrey','language'=>'en_us','reminder_interval'=>'1 Minute','currency_name'=>'Japan, Yen','currency_code'=>'JPY','currency_symbol'=>'¥','conv_rate'=>'1.000','record_id'=>'','record_module'=>'','no_of_currency_decimals'=>'2','id'=>'1');
+
+?>
