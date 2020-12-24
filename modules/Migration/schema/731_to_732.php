401f46ceec2b3db06add0f5940af268d7e97bb20
sota_miyamoto <miyamoto@thinkingreed.co.jp>
2020-12-24 14:57:43 +0900

    カレンダーのエクスポートでヘッダーの順番がvalueと一致するように修正

--- modules/Migration/schema/731_to_732.php
+++ modules/Migration/schema/731_to_732.php
@@ -14,4 +14,26 @@ if (defined('VTIGER_UPGRADE')) {
 
 	//課税計算の手数料額をnullから0円に変更
 	$db->query("UPDATE vtiger_inventorycharges SET value = 0 WHERE value IS NULL;");
+
+	//カレンダーのエクスポートでヘッダーの順番を変更(tabid = 9 && block = 19のsequenceを整頓)
+	$query = "SELECT columnname,fieldid,sequence FROM vtiger_field WHERE tabid = 9 AND block = (SELECT block FROM vtiger_field WHERE tabid = 9 AND columnname = 'time_start') ORDER BY sequence;";
+	$result = $db->query($query);
+	$result_num = $db->num_rows($result);
+	for ($j = 0; $j < $result_num; $j++) {
+		$columnname = $db->query_result($result, $j,"columnname");
+		$fieldid = intval($db->query_result($result, $j,"fieldid"));
+		$sequence = intval($db->query_result($result, $j,"sequence"));
+		$fieldData[$columnname] = array(
+			"fieldid" => $fieldid,
+			"sequence" => $sequence,
+		);
+	}
+	if($fieldData["time_start"]["sequence"] == $fieldData["time_end"]["sequence"]){
+		$i = 0;
+		foreach($fieldData as $key => $value){
+			$updateQuery = "UPDATE vtiger_field SET sequence = ".($i+1)."  WHERE fieldid = ".$value['fieldid']." AND tabid = 9;";
+			$db->query($updateQuery);
+			$i++;
+		}
+	}
 }
