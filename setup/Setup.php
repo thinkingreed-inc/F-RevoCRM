164de94570e24ae9fce58a767128f387e15660f8
Remicck <remicck@gmail.com>
2021-06-23 21:39:30 +0900

    fixed #79 プロジェクトタスク「終了日」のクイック作成をデフォルトで有効になるように修正

--- setup/Setup.php
+++ setup/Setup.php
@@ -74,6 +74,9 @@ require_once ("scripts/54_Update_SidebarWidget.php");
 // 日本語を追加
 require_once ("scripts/56_Add_JapaneseLanguage.php");
 
+// プロジェクトタスクの終了日をクリッククリエイトに追加
+$db->query('update vtiger_field set quickcreate = 0 where tabid = 41 and fieldname = "enddate"');
+
 // メニュー設定
 // FRMenuSetting::apply(array(
 //     'Accounts',
