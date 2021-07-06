17e0385918219db18c5f015d9df087ac53705ca8
Remicck <remicck@gmail.com>
2021-07-06 10:38:03 +0900

    fixed #194 SQLの不具合を修正

--- modules/Migration/schema/733_to_734.php
+++ modules/Migration/schema/733_to_734.php
@@ -17,7 +17,7 @@ if (defined('VTIGER_UPGRADE')) {
     $f_result = $db->query('select f.fieldid from vtiger_field f left join vtiger_tab t on f.tabid = t.tabid where t.name = "ProjectMilestone" and f.fieldname = "projectid" limit 1');
     $f_count = $db->num_rows($f_result);
     // relation_id
-    $r_result = $db->query('select relation_id from vtiger_relatedlists where name = "get_dependents_list" and label = "Project Milestone" limit 1');
+    $r_result = $db->query('select relation_id from vtiger_relatedlists where name = "get_dependents_list" and label = "Project Milestones" limit 1');
     $r_count = $db->num_rows($r_result);
     if($f_count > 0 && $r_count > 0){
         $fieldid = $db->query_result($f_result, 0, 'fieldid');
