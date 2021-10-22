075390eeb01bb18934761bdcc7be76c17bf385fa
pavish69 <pavish.yac03@gmail.com>
2021-10-22 16:33:08 +0900

    fix #311 他の人がインポート中のときにインポートしようとする時に表示される文言の翻訳

--- languages/ja_jp/Vtiger.php
+++ languages/ja_jp/Vtiger.php
@@ -1611,6 +1611,8 @@ $jsLanguageStrings = array(
 	'JS_PLEASE_ENTER_SOME_VALUE' => '検索する値をいくつか入力してください',
 	'JS_VCF_FILE_IMPORT_WARNING' => 'VCFファイルが選択されました。VCFファイルからインポートできるのは顧客担当者のみです。このファイルをインポートするには顧客担当者からインポートを開始してください。',
 	'JS_ICS_FILE_IMPORT_WARNING' => 'ICSファイルが選択されました。ICSファイルからインポートできるのはカレンダーのみです。このファイルをインポートするには、カレンダーモジュールからインポートを開始してください',
+	'LBL_IMPORT_ERROR_MULTIPLE_ACCOUNTS' => '%s が %s をインポートしています。時間をおいて再度お試しください。',
+
 	//Select2 elements validation message
 	'JS_YOU_CAN_SELECT_ONLY'=> '選択可能：',
 	'JS_ITEMS' => '項目',
