eb6d4780a5d54ec9c61a41ac462ded967598aa57
kanno <kanno@thinkingreed.sakura.ne.jp>
2021-12-09 11:18:39 +0900

    #371 マイグレーションでINDEXが効いていないuitype10(関連項目)にINDEXを追加する修正。同じクエリが何度も走る設計になっている

--- modules/Migration/schema/735_to_736.php
+++ modules/Migration/schema/735_to_736.php
@@ -13,4 +13,19 @@ if (defined('VTIGER_UPGRADE')) {
     $db = PearDatabase::getInstance();
     // 契約モジュールにprimary keyを追加
     $db->pquery("alter table vtiger_servicecontracts add primary key (servicecontractsid)", array());
+
+    //uitype10にindexが付与される修正を加えた
+    //しかし、過去に作成したものはそのままであるためバージョンアップ時に付与する
+    $result = $adb->pquery("SELECT tablename,columnname FROM vtiger_field WHERE uitype=10");
+    $rows = $adb->num_rows($result);
+    for ($i = 0; $i < $rows; $i++) {
+        $tablename = $adb->query_result($result, $i, 'tablename');
+        $columnname = $adb->query_result($result, $i, 'columnname');
+        
+        $result_index = $adb->pquery('SHOW INDEX FROM '.$tablename.' WHERE Column_name="'.$columnname.'"');
+        $rows_index = $adb->num_rows($result_index);
+        if($rows_index == 0){
+            $adb->pquery("CREATE INDEX ".$columnname." ON ".$tablename."(".$columnname.")");
+        }
+    }
 }
