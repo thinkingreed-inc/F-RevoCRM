1a33dc96faf524e35fecd279cdef55dab970f536
kanno <kanno@thinkingreed.sakura.ne.jp>
2021-12-09 13:05:38 +0900

    uitype10以外でもインデックスが付与されるように修正。同じクエリが連続して飛ばないように修正

--- modules/Migration/schema/735_to_736.php
+++ modules/Migration/schema/735_to_736.php
@@ -15,17 +15,25 @@ if (defined('VTIGER_UPGRADE')) {
     $db->pquery("alter table vtiger_servicecontracts add primary key (servicecontractsid)", array());
 
     //uitype10にindexが付与される修正を加えた
-    //しかし、過去に作成したものはそのままであるためバージョンアップ時に付与する
-    $result = $adb->pquery("SELECT tablename,columnname FROM vtiger_field WHERE uitype=10");
+    //しかし過去に作成したものはそのままであるため、バージョンアップ時に付与する
+    $result = $adb->pquery('SELECT f.tablename, f.columnname
+        FROM vtiger_fieldmodulerel AS fmrel
+        LEFT JOIN vtiger_field AS f
+        ON fmrel.fieldid = f.fieldid
+        WHERE f.tablename != "NULL"
+        ORDER BY f.tablename, f.columnname;');
     $rows = $adb->num_rows($result);
     for ($i = 0; $i < $rows; $i++) {
         $tablename = $adb->query_result($result, $i, 'tablename');
         $columnname = $adb->query_result($result, $i, 'columnname');
-        
-        $result_index = $adb->pquery('SHOW INDEX FROM '.$tablename.' WHERE Column_name="'.$columnname.'"');
-        $rows_index = $adb->num_rows($result_index);
-        if($rows_index == 0){
-            $adb->pquery("CREATE INDEX ".$columnname." ON ".$tablename."(".$columnname.")");
+        if($tablename != $tablename_old || $columnname != $columnname_old){ //同じクエリが連続して飛ぶケースを除外する
+            $result_index = $adb->pquery('SHOW INDEX FROM '.$tablename.' WHERE Column_name="'.$columnname.'"');
+            $rows_index = $adb->num_rows($result_index);
+            if($rows_index == 0){
+                $adb->pquery("CREATE INDEX ".$columnname." ON ".$tablename."(".$columnname.")");
+            }
         }
+        $tablename_old = $tablename;
+        $columnname_old = $columnname;
     }
 }
