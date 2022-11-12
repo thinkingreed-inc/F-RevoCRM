63f771d3815f48d0b347b27b624bbb5bc227b112
koseki <koseki@thinkingreed.co.jp>
2022-11-12 18:34:56 +0900

    refs #629 初期設定でメールの「クリック数」のカウントをオンの追加修正

--- config.template.php
+++ config.template.php
@@ -185,8 +185,8 @@ $default_layout = 'v7';
 // スケジュールワークフローの設定最大数
 $max_scheduled_workflows = 50;
 
-include_once 'config.security.php';
-
 // メールの「クリック数」のカウント
 $email_tracking = 'Yes';
+
+include_once 'config.security.php';
 ?>
