e4e5d2b85e3d8129db00fab70958693768526c8f
junmt <junmt.b@gmail.com>
2023-08-29 17:49:19 +0900

    v7.3.9のバージョン情報を追加

--- vtigerversion.php
+++ vtigerversion.php
@@ -10,7 +10,7 @@
 
 $patch_version = '20230508'; // -ve timestamp before release, +ve timestamp after release.
 $modified_database = '';
-$vtiger_current_version = '7.3.8.1';
+$vtiger_current_version = '7.3.9';
 $_SESSION['vtiger_version'] = $vtiger_current_version;
 
 ?>
