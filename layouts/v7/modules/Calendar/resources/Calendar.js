212ef9711a7dc19ca7d8229a2df3f7046d8ab840
KentoYano <107910164+kento-yn@users.noreply.github.com>
2023-08-29 16:51:16 +0900

    #744 マイグループのチェックボックスが変化しないよう修正 (#758)

--- layouts/v7/modules/Calendar/resources/Calendar.js
+++ layouts/v7/modules/Calendar/resources/Calendar.js
@@ -1270,7 +1270,7 @@ Vtiger.Class("Calendar_Calendar_Js", {
 				thisInstance.updateAllEventsOnCalendar();
 			}
 			if(thisInstance.changeUserList) {
-				thisInstance.changeUserList();
+				//thisInstance.changeUserList();
 			}
 		});
 	},
