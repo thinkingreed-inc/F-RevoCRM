61166d4d916f161a01727ee3dc79a334a4bfdc42
junmt <12598156+junmt@users.noreply.github.com>
2023-12-06 18:42:29 +0900

    READMEを7.4.0に変更とゴミ箱の不具合を修正 (#911)
    * PHP8対応：選択肢の連動設定がエラーになっていたため修正
    
    * READMEを7.4.0に変更とゴミ箱の不具合を修正
    
    ---------
    
    Co-authored-by: Jun Matsuda <{ID}+{username}@users.noreply.github.com>

--- modules/RecycleBin/views/List.php
+++ modules/RecycleBin/views/List.php
@@ -176,7 +176,7 @@ class RecycleBin_List_View extends Vtiger_Index_View {
 			$this->pagingModel = $pagingModel;
 		}
 
-		$noOfEntries = count($this->listViewEntries);
+		$noOfEntries = php7_count($this->listViewEntries);
 
 		$viewer->assign('MODULE', $moduleName);
 
