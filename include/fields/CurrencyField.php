142235a2c8c3430bad81cc816138cea3237a19e6
Jun Matsuda <{ID}+{username}@users.noreply.github.com>
2023-12-12 15:19:30 +0900

    [不具合] PHP8対応：金額計算時にエラーになる #927

--- include/fields/CurrencyField.php
+++ include/fields/CurrencyField.php
@@ -363,7 +363,7 @@ class CurrencyField {
 		}
 		//$value = round($value, $this->maxNumberOfDecimals);
 
-        return $value;
+        return (float)$value;
     }
 
     /**
@@ -415,7 +415,7 @@ class CurrencyField {
 	
 	public static function convertFromDollar($amount, $conversionRate) {
 		$currencyField = new CurrencyField($amount);
-		return round($amount * $conversionRate, $currencyField->maxNumberOfDecimals);
+		return round( (float)$amount * (float)$conversionRate, $currencyField->maxNumberOfDecimals);
 	}
 	
 	/** This function returns the amount converted from master currency.
@@ -423,7 +423,7 @@ class CurrencyField {
 	 * param $crate - conversion rate.
 	 */
 	public static function convertFromMasterCurrency($amount, $conversionRate) {
-		return $amount * $conversionRate;
+		return (float)$amount * (float)$conversionRate;
 	}
 	
 	function currencyDecimalFormat($value, $user = null){
@@ -464,4 +464,3 @@ class CurrencyField {
 		}
 	}
 }
-?>
