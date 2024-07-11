0d0e58f22aee22393ca2d0c207a0548bdcc392e4
sanototsuka <tots.2.105@gmail.com>
2024-07-11 13:47:57 +0900

    クイック作成の複数選択肢の値を整形して詳細に送るよう修正

--- layouts/v7/modules/Vtiger/resources/Vtiger.js
+++ layouts/v7/modules/Vtiger/resources/Vtiger.js
@@ -471,6 +471,13 @@ Vtiger.Class('Vtiger_Index_Js', {
 		// formDataの中身をhiddenのinputにしてeditFormに入れる
 		for(var key in formData) {
 			var value = formData[key];
+			// 複数選択肢の場合のみ
+			if ( jQuery('select[name="'+key+'"]').length && key.includes("[]") ) {
+				key = key.replace("[]", "");
+				if(Array.isArray(value)) {
+					value = value.join(" |##| ");
+				}
+			}
 			var input = document.createElement('input');
 			input.type = 'hidden';
 			input.name = key;
