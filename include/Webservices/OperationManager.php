d1161a85f846671201330c7e0662a420e2f4d7d4
Remicck <remicck@gmail.com>
2024-09-10 19:07:18 +0900

    fix: Retrieve APIを修正

--- include/Webservices/OperationManager.php
+++ include/Webservices/OperationManager.php
@@ -152,7 +152,7 @@
 			try{
 				$operation = strtolower($this->operationName);
 				if(!$this->preLogin){
-					$params[] = $user;
+					$params['user'] = $user;
 					return call_user_func_array($this->handlerMethod,$params);
 				}else{
 					$userDetails = call_user_func_array($this->handlerMethod,$params);
