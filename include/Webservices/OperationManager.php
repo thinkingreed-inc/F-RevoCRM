f0a1d1be47f9b362b09319a8b40979ba1493833b
Remicck <remicck@gmail.com>
2024-09-11 13:44:03 +0900

    chore: 不要なコメントアウトの削除

--- include/Webservices/OperationManager.php
+++ include/Webservices/OperationManager.php
@@ -125,9 +125,7 @@
 			$sanitizedInput = array();
 			foreach($this->operationParams as $ind=>$columnDetails){
 				foreach ($columnDetails as $columnName => $type) {
-					// if(isset($input, $columnName)){
-						$sanitizedInput[$columnName] = $this->handleType($type,vtws_getParameter($input,$columnName));
-					// }
+					$sanitizedInput[$columnName] = $this->handleType($type,vtws_getParameter($input,$columnName));
 				}
 			}
 			return $sanitizedInput;
