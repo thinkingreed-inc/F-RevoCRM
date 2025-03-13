0ac2c698349ed4fa047547dc40dfe9ca2aaec8da
Taisho Iwamoto <iwamoto@thinkingreed.co.jp>
2025-03-13 20:59:58 +0900

    カレンダーの参加者一覧の下にスペースを追加

--- layouts/v7/modules/Calendar/resources/Calendar.js
+++ layouts/v7/modules/Calendar/resources/Calendar.js
@@ -1677,7 +1677,7 @@ Vtiger.Class("Calendar_Calendar_Js", {
 			}
 
 			if(eventObj.description && eventObj.description != '') {
-				popOverHTML += eventObj.description;
+				popOverHTML += '<div class="description-info">' + eventObj.description + '</div>';
 				popOverHTML += '<br>';
 			}
 			if(event.creator && event.creator != '') {
