1d8b41f39c88d9a83961063c9446e3b44a4a4f83
kashiwakura <kashiwakura@thinkingreed.co.jp>
2025-05-16 14:22:50 +0900

    thinkingreed-inc#1258 PDFテンプレート内に初期データを入れる際vtranslateをかけるように修正。 また翻訳追加時の不具合も合わせて修正。

--- setup/scripts/17_Update_Inventory.php
+++ setup/scripts/17_Update_Inventory.php
@@ -37,5 +37,5 @@ $record->save();
 // $adb->pquery("INSERT INTO vtiger_inventorytaxinfo(taxid, taxname, taxlabel, percentage, deleted, method, type, compoundon, regions)values(?, ?, ?, ?, ?, ?, ?, ?, ?)"
 //             ,array(4, 'tax4', '消費税', 10.000, 0, 'Simple', 'Fixed', '[]', '[]'));
 
-$adb->pquery("INSERT INTO vtiger_taxregions(regionid, name)values(?, ?)", array('1', '日本'));
+$adb->pquery("INSERT INTO vtiger_taxregions(regionid, name)values(?, ?)", array('1', 'LBL_JAPAN'));
 
