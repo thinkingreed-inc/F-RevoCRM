18dffd22b0153ddeaf14ba9dde06349434f31dad
kashiwakura <kashiwakura@thinkingreed.co.jp>
2025-06-24 10:24:23 +0900

    thinkingreed-inc#1286 github板で不要と思われる言語変数の削除

--- languages/en_us/Settings/Parameters.php
+++ languages/en_us/Settings/Parameters.php
@@ -15,15 +15,6 @@ $languageStrings = array(
 	'LBL_EDIT_MODULE' => 'Edit System Variables',
 	'LBL_ADD_NEW_MODULE' => 'Add System Variables',
 	'LBL_DUPLICATE_KEY' => 'The key already exists. Please specify a different key.',
-	'LBL_GOOGLE_CALENDAR_CLIENT_ID_DESCRIPTION' => 'Client ID for Google Calendar integration',
-	'LBL_GOOGLE_CALENDAR_CLIENT_SECRET_DESCRIPTION' => 'Client Secret for Google Calendar integration',
-	'LBL_MICROSOFT_APP_CLIENT_ID_DESCRIPTION' => 'Client ID for Microsoft365 integration',
-	'LBL_MICROSOFT_APP_SECRET_DESCRIPTION' => 'Client Secret for Microsoft365 integration',
-	'LBL_MICROSOFT_APP_TENANT_ID_DESCRIPTION' => 'Tenant ID for Microsoft365 integration (leave blank if enabling personal account)',
-	'LBL_USE_SSO_DESCRIPTION' => 'Single Sign-On Enabled Flag [Note: If you enable it and there is an error in your settings, you will not be able to log in!]',
-	'LBL_SSO_URL_DESCRIPTION' => 'Single sign-on authentication URL, specifies the URL of the login authentication screen of the single sign-on system.',
-	'LBL_SSO_SAML_SP_NAME_DESCRIPTION' => 'Enter the SP name. Normally, this should not be changed. If you need to change it due to special circumstances, such as program modification, etc., please change it.',
-	'LBL_LOGOUT_PAGE_URL' => 'Single logout page URL',	
 );
 
 $jsLanguageStrings = array(
--- languages/ja_jp/Settings/Parameters.php
+++ languages/ja_jp/Settings/Parameters.php
@@ -15,15 +15,6 @@ $languageStrings = array(
 	'LBL_EDIT_MODULE' => 'システム変数の編集',
 	'LBL_ADD_NEW_MODULE' => 'システム変数の追加',
 	'LBL_DUPLICATE_KEY' => '既に存在するキーと重複しています。異なるキーを指定してください。',
-	'LBL_GOOGLE_CALENDAR_CLIENT_ID_DESCRIPTION' => 'Googleカレンダー連携用のクライアントID',
-	'LBL_GOOGLE_CALENDAR_CLIENT_SECRET_DESCRIPTION' => 'Googleカレンダー連携用のクライアントシークレット',
-	'LBL_MICROSOFT_APP_CLIENT_ID_DESCRIPTION' => 'Microsoft365連携用のクライアントID',
-	'LBL_MICROSOFT_APP_SECRET_DESCRIPTION' => 'Client Secret for Microsoft365 integration',
-	'LBL_MICROSOFT_APP_TENANT_ID_DESCRIPTION' => 'Microsoft365連携用のテナントID(個人用アカウントを有効にする場合は空欄にする)',
-	'LBL_USE_SSO_DESCRIPTION' => 'シングルサインオン有効フラグ【注意：有効にして設定に誤りがある場合はログインできなくなります！】',
-	'LBL_SSO_URL_DESCRIPTION' => 'シングルサインオン認証URL、シングルサインオンのシステムのログイン認証画面のURLを指定します。',
-	'LBL_SSO_SAML_SP_NAME_DESCRIPTION' => 'F-RevoCRM側のSP名を入力します。通常変更することはありません。特殊な事情でプログラム側の改修等により変更が必要な場合は変更してください。',
-	'LBL_LOGOUT_PAGE_URL' => 'シングルログアウトページURL',	
 );
 
 $jsLanguageStrings = array(
