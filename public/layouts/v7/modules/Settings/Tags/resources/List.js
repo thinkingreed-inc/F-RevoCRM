65825f627f47af800b4142da4a8eb30370ac09fe
chinseika <chin@thinkingreed.co.jp>
2025-11-11 17:59:48 +0900

    #1282 　2つエラーメッセージが表示エラー修正

--- public/layouts/v7/modules/Settings/Tags/resources/List.js
+++ public/layouts/v7/modules/Settings/Tags/resources/List.js
@@ -98,8 +98,9 @@ Settings_Vtiger_List_Js('Settings_Tags_List_Js',{
     registerEditTagSaveEvent : function() {
         var editTagContainer = this.getEditTagContainer();
         var self = this;
-        this.getEditTagContainer().find('.saveTag').on('click', function(e){
-            
+
+        this.getEditTagContainer().find('.saveTag').off('click').on('click', function(e){
+
             var tagName = editTagContainer.find('[name="tagName"]').val();
             if(tagName.trim() == ""){
                 var message = app.vtranslate('JS_PLEASE_ENTER_VALID_TAG_NAME');
