aaa9445bc5762b1b62a702b45d2f7b49d569c683
chinseika <chin@thinkingreed.co.jp>
2025-11-20 18:42:41 +0900

    #1333　モジュール名変更対象となるテーブル追加

--- setup/migration/scripts/20251111191427_update_vtiger_tab_name_length.php
+++ setup/migration/scripts/20251111191427_update_vtiger_tab_name_length.php
@@ -6,19 +6,28 @@
 
 require_once dirname(__FILE__) . '/../FRMigrationClass.php';
 
-class Migration20251111191427_UpdateVtigerTabNameLength extends FRMigrationClass {
+class Migration20251111191427_UpdateModuleNameFieldLength extends FRMigrationClass {
     
-    /**
-     * マイグレーションを実行する
-     * ここにマイグレーション処理を記述してください
-     */
     public function process() {
-        global $current_user, $adb;
         $db = PearDatabase::getInstance();
 
+        // vtiger_tab.name
         $sql = "ALTER TABLE vtiger_tab MODIFY COLUMN name VARCHAR(50) NOT NULL";
-         $db->pquery($sql, array());
+        $db->pquery($sql, array());
 
-        $this->log("マイグレーション update_vtiger_tab_name_length が正常に完了しました");
+        // vtiger_tracker.module_name
+        $sql = "ALTER TABLE vtiger_tracker MODIFY COLUMN module_name VARCHAR(50) NOT NULL";
+        $db->pquery($sql, array());
+
+        // vtiger_ws_referencetype.type
+        $sql = "ALTER TABLE vtiger_ws_referencetype MODIFY COLUMN type VARCHAR(50) NOT NULL";
+        $db->pquery($sql, array());
+
+        // vtiger_ws_entity.name
+        $sql = "ALTER TABLE vtiger_ws_entity MODIFY COLUMN name VARCHAR(50) NOT NULL";
+        $db->pquery($sql, array());
+
+        $this->log("マイグレーション update_field_length_to_50 が正常に完了しました");
     }
+
 }
