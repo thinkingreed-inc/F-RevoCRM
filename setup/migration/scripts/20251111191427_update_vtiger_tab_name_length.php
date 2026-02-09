c5741f94caec70a73b3b6836fd2583e164c90bdd
chinseika <chin@thinkingreed.co.jp>
2026-02-09 10:59:05 +0900

    関連テーブル修正を追加する

--- setup/migration/scripts/20251111191427_update_vtiger_tab_name_length.php
+++ setup/migration/scripts/20251111191427_update_vtiger_tab_name_length.php
@@ -27,6 +27,14 @@ class Migration20251111191427_UpdateModuleNameFieldLength extends FRMigrationCla
         $sql = "ALTER TABLE vtiger_ws_entity MODIFY COLUMN name VARCHAR(50) NOT NULL";
         $db->pquery($sql, array());
 
+        // vtiger_ws_entity_referencetype.type
+        $sql = "ALTER TABLE vtiger_ws_entity_referencetype MODIFY COLUMN type VARCHAR(50) NOT NULL";
+        $db->pquery($sql, array());
+
+        // vtiger_customview.entitytype
+        $sql = "ALTER TABLE vtiger_customview MODIFY COLUMN entitytype VARCHAR(50) NOT NULL";
+        $db->pquery($sql, array());
+
         $this->log("マイグレーション update_field_length_to_50 が正常に完了しました");
     }
 
